2049a5391e25318774210e1abb5f97cf5bbd77c4
PROBOY <71318599+pro-boy@users.noreply.github.com>
2021-01-04 11:22:05 +0530

    Fixed 👍🛡

--- CyberRobot/modules/chatbot.py
+++ CyberRobot/modules/chatbot.py
@@ -2,14 +2,14 @@ import html
 # AI module using Intellivoid's Coffeehouse API by @TheRealPhoenix
 from time import sleep, time
 
-import SaitamaRobot.modules.sql.chatbot_sql as sql
+import CyberRobot.modules.sql.chatbot_sql as sql
 from coffeehouse.api import API
 from coffeehouse.exception import CoffeeHouseError as CFError
 from coffeehouse.lydia import LydiaAI
-from SaitamaRobot import AI_API_KEY, OWNER_ID, SUPPORT_CHAT, dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
-from SaitamaRobot.modules.helper_funcs.filters import CustomFilters
-from SaitamaRobot.modules.log_channel import gloggable
+from CyberRobot import AI_API_KEY, OWNER_ID, SUPPORT_CHAT, dispatcher
+from CyberRobot.modules.helper_funcs.chat_status import user_admin
+from CyberRobot.modules.helper_funcs.filters import CustomFilters
+from CyberRobot.modules.log_channel import gloggable
 from telegram import Update
 from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import (CallbackContext, CommandHandler, Filters,
@@ -136,7 +136,7 @@ def list_chatbot_chats(update: Update, context: CallbackContext):
 
 
 __help__ = f"""
-Chatbot utilizes the CoffeeHouse API and allows Saitama to talk and provides a more interactive group chat experience.
+Chatbot utilizes the CoffeeHouse API and allows Cyber Robot to talk and provides a more interactive group chat experience.
 
 *Commands:* 
 *Admins only:*
